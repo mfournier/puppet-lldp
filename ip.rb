1f671488c10d089e9dd292102c2f29318e9e1648
Marc Fournier <marc.fournier@camptocamp.com>
2012-06-27 15:34:25 +0200

    rm'ed ip & ipmi facts.

--- ip.rb
+++
@@ -1,7 +0,0 @@
-if File::exists?("/sbin/ip")
-  Facter.add('gateway') do
-    setcode do
-      `/sbin/ip route show`.match(/^default.*/)[0].match(/(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/)[0]
-    end
-  end
-end
